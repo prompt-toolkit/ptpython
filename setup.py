71cb33319f899d36cfa61fbb97b1c380fed837ba
Jonathan Slenders <jonathan@slenders.be>
2015-06-18 20:12:16 +0200

    Pypi release 0.14

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ long_description = open(
 setup(
     name='ptpython',
     author='Jonathan Slenders',
-    version='0.13',
+    version='0.14',
     url='https://github.com/jonathanslenders/ptpython',
     description='Python REPL build on top of prompt_toolkit',
     long_description=long_description,
     packages=find_packages('.'),
     install_requires = [
-        'prompt_toolkit==0.39',
+        'prompt_toolkit==0.40',
         'jedi>=0.9.0',
         'docopt',
     ],
