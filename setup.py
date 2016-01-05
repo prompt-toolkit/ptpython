8b0dfad0b77e70afc1660f67c193738793dfee0c
Jonathan Slenders <jonathan@slenders.be>
2016-01-05 06:06:59 +0100

    Release 0.28

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ long_description = open(
 setup(
     name='ptpython',
     author='Jonathan Slenders',
-    version='0.27',
+    version='0.28',
     url='https://github.com/jonathanslenders/ptpython',
     description='Python REPL build on top of prompt_toolkit',
     long_description=long_description,
     packages=find_packages('.'),
     install_requires = [
-        'prompt_toolkit==0.56',
+        'prompt_toolkit==0.57',
         'jedi>=0.9.0',
         'docopt',
     ],
