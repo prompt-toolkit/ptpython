77e6e6244cc716eec0d1f842b3d735a4d43e7087
zlsun <zlsun1995@gmail.com>
2016-06-07 12:08:52 +0800

    Use `sys.version_info` instead of `sys.version`

--- setup.py
+++ setup.py
@@ -29,10 +29,10 @@ setup(
         'console_scripts': [
             'ptpython = ptpython.entry_points.run_ptpython:run',
             'ptipython = ptpython.entry_points.run_ptipython:run',
-            'ptpython%s = ptpython.entry_points.run_ptpython:run' % sys.version[:1],
-            'ptpython%s = ptpython.entry_points.run_ptpython:run' % sys.version[:3],
-            'ptipython%s = ptpython.entry_points.run_ptipython:run' % sys.version[:1],
-            'ptipython%s = ptpython.entry_points.run_ptipython:run' % sys.version[:3],
+            'ptpython%s = ptpython.entry_points.run_ptpython:run' % sys.version_info[0],
+            'ptpython%s.%s = ptpython.entry_points.run_ptpython:run' % sys.version_info[:2],
+            'ptipython%s = ptpython.entry_points.run_ptipython:run' % sys.version_info[0],
+            'ptipython%s.%s = ptpython.entry_points.run_ptipython:run' % sys.version_info[:2],
         ]
     },
     extra_require={
