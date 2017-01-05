68bf87ba791aaed336546341729f571a01f691ec
Rob Phoenix <robphoenix@protonmail.com>
2017-01-05 11:22:32 +0000

    Make default config dir filepath OS independent.

--- ptpython/entry_points/run_ptpython.py
+++ ptpython/entry_points/run_ptpython.py
@@ -29,7 +29,7 @@ def run():
     a = docopt.docopt(__doc__)
 
     vi_mode = bool(a['--vi'])
-    config_dir = os.path.expanduser(a['--config-dir'] or '~/.ptpython/')
+    config_dir = os.path.expanduser(a['--config-dir'] or os.path.join('~', '.ptpython'))
 
     # Create config directory.
     if not os.path.isdir(config_dir) or not os.path.islink(config_dir):
