e6e91b66e4cd1e2d60815d797ccf0a669cfb0caa
Jonathan Slenders <jonathan@slenders.be>
2017-03-26 21:02:30 +0200

    Release 0.38

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = open(
 setup(
     name='ptpython',
     author='Jonathan Slenders',
-    version='0.37',
+    version='0.38',
     url='https://github.com/jonathanslenders/ptpython',
     description='Python REPL build on top of prompt_toolkit',
     long_description=long_description,
