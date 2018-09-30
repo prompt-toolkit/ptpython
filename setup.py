db24a15035e924081aaf41e1cc7ec181f9b23c49
Jonathan Slenders <jslender@cisco.com>
2018-09-30 20:27:42 +0200

    Release 2.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = open(
 setup(
     name='ptpython',
     author='Jonathan Slenders',
-    version='0.36',
+    version='2.0.1',
     url='https://github.com/jonathanslenders/ptpython',
     description='Python REPL build on top of prompt_toolkit',
     long_description=long_description,
