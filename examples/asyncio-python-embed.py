faeaf7852fcaa44f2ca741e1981e37d20f145d24
Mikhail Terekhov <termim@gmail.com>
2020-10-05 11:53:23 -0400

    Update asyncio-python-embed.py
    Fix deprecation warning in Python 3.8.

--- examples/asyncio-python-embed.py
+++ examples/asyncio-python-embed.py
@@ -19,19 +19,17 @@ loop = asyncio.get_event_loop()
 counter = [0]
 
 
-@asyncio.coroutine
-def print_counter():
+async def print_counter():
     """
     Coroutine that prints counters and saves it in a global variable.
     """
     while True:
         print("Counter: %i" % counter[0])
         counter[0] += 1
-        yield from asyncio.sleep(3)
+        await asyncio.sleep(3)
 
 
-@asyncio.coroutine
-def interactive_shell():
+async def interactive_shell():
     """
     Coroutine that starts a Python REPL from which we can access the global
     counter variable.
@@ -40,7 +38,7 @@ def interactive_shell():
         'You should be able to read and update the "counter[0]" variable from this shell.'
     )
     try:
-        yield from embed(
+        await embed(
             globals=globals(), return_asyncio_coroutine=True, patch_stdout=True
         )
     except EOFError:
