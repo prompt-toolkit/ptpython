107aba8dea556207389227c6789043e50c65ff95
Jonathan Slenders <jonathan@slenders.be>
2021-01-28 12:18:32 +0100

    Cleanup of completion code.

--- ptpython/completer.py
+++ ptpython/completer.py
@@ -51,6 +51,7 @@ class PythonCompleter(Completer):
         self.get_enable_dictionary_completion = get_enable_dictionary_completion
 
         self._system_completer = SystemCompleter()
+        self._jedi_completer = JediCompleter(get_globals, get_locals)
         self._dictionary_completer = DictionaryCompleter(get_globals, get_locals)
 
         self._path_completer_cache: Optional[GrammarCompleter] = None
@@ -129,10 +130,14 @@ class PythonCompleter(Completer):
         )
 
     def _complete_python_while_typing(self, document: Document) -> bool:
-        char_before_cursor = document.char_before_cursor
+        """
+        When `complete_while_typing` is set, only return completions when this
+        returns `True`.
+        """
+        text = document.text_before_cursor.rstrip()
+        char_before_cursor = text[-1:]
         return bool(
-            document.text
-            and (char_before_cursor.isalnum() or char_before_cursor in "_.")
+            text and (char_before_cursor.isalnum() or char_before_cursor in "_.(,")
         )
 
     def get_completions(
@@ -151,94 +156,127 @@ class PythonCompleter(Completer):
             )
             return
 
-        # Do dictionary key completions.
-        if self.get_enable_dictionary_completion():
-            has_dict_completions = False
-            for c in self._dictionary_completer.get_completions(
-                document, complete_event
-            ):
-                if c.text not in "[.":
-                    # If we get the [ or . completion, still include the other
-                    # completions.
-                    has_dict_completions = True
-                yield c
-            if has_dict_completions:
-                return
-
         # Do Path completions (if there were no dictionary completions).
         if complete_event.completion_requested or self._complete_path_while_typing(
             document
         ):
             yield from self._path_completer.get_completions(document, complete_event)
 
-        # If we are inside a string, Don't do Jedi completion.
-        if self._path_completer_grammar.match(document.text_before_cursor):
-            return
-
-        # Do Jedi Python completions.
         if complete_event.completion_requested or self._complete_python_while_typing(
             document
         ):
-            script = get_jedi_script_from_document(
-                document, self.get_locals(), self.get_globals()
-            )
+            # If we are inside a string, Don't do Python completion.
+            if self._path_completer_grammar.match(document.text_before_cursor):
+                return
 
-            if script:
-                try:
-                    jedi_completions = script.complete(
-                        column=document.cursor_position_col,
-                        line=document.cursor_position_row + 1,
+            # Do dictionary key completions.
+            if self.get_enable_dictionary_completion():
+                has_dict_completions = False
+                for c in self._dictionary_completer.get_completions(
+                    document, complete_event
+                ):
+                    if c.text not in "[.":
+                        # If we get the [ or . completion, still include the other
+                        # completions.
+                        has_dict_completions = True
+                    yield c
+                if has_dict_completions:
+                    return
+
+            # Do Jedi Python completions.
+            yield from self._jedi_completer.get_completions(document, complete_event)
+
+
+class JediCompleter(Completer):
+    """
+    Autocompleter that uses the Jedi library.
+    """
+
+    def __init__(self, get_globals, get_locals) -> None:
+        super().__init__()
+
+        self.get_globals = get_globals
+        self.get_locals = get_locals
+
+    def get_completions(
+        self, document: Document, complete_event: CompleteEvent
+    ) -> Iterable[Completion]:
+        script = get_jedi_script_from_document(
+            document, self.get_locals(), self.get_globals()
+        )
+
+        if script:
+            try:
+                jedi_completions = script.complete(
+                    column=document.cursor_position_col,
+                    line=document.cursor_position_row + 1,
+                )
+            except TypeError:
+                # Issue #9: bad syntax causes completions() to fail in jedi.
+                # https://github.com/jonathanslenders/python-prompt-toolkit/issues/9
+                pass
+            except UnicodeDecodeError:
+                # Issue #43: UnicodeDecodeError on OpenBSD
+                # https://github.com/jonathanslenders/python-prompt-toolkit/issues/43
+                pass
+            except AttributeError:
+                # Jedi issue #513: https://github.com/davidhalter/jedi/issues/513
+                pass
+            except ValueError:
+                # Jedi issue: "ValueError: invalid \x escape"
+                pass
+            except KeyError:
+                # Jedi issue: "KeyError: u'a_lambda'."
+                # https://github.com/jonathanslenders/ptpython/issues/89
+                pass
+            except IOError:
+                # Jedi issue: "IOError: No such file or directory."
+                # https://github.com/jonathanslenders/ptpython/issues/71
+                pass
+            except AssertionError:
+                # In jedi.parser.__init__.py: 227, in remove_last_newline,
+                # the assertion "newline.value.endswith('\n')" can fail.
+                pass
+            except SystemError:
+                # In jedi.api.helpers.py: 144, in get_stack_at_position
+                # raise SystemError("This really shouldn't happen. There's a bug in Jedi.")
+                pass
+            except NotImplementedError:
+                # See: https://github.com/jonathanslenders/ptpython/issues/223
+                pass
+            except Exception:
+                # Supress all other Jedi exceptions.
+                pass
+            else:
+                # Move function parameters to the top.
+                jedi_completions = sorted(
+                    jedi_completions,
+                    key=lambda jc: (
+                        # Params first.
+                        jc.type != "param",
+                        # Private at the end.
+                        jc.name.startswith("_"),
+                        # Then sort by name.
+                        jc.name_with_symbols.lower(),
+                    ),
+                )
+
+                for jc in jedi_completions:
+                    if jc.type == "function":
+                        suffix = "()"
+                    else:
+                        suffix = ""
+
+                    if jc.type == "param":
+                        suffix = "..."
+
+                    yield Completion(
+                        jc.name_with_symbols,
+                        len(jc.complete) - len(jc.name_with_symbols),
+                        display=jc.name_with_symbols + suffix,
+                        display_meta=jc.type,
+                        style=_get_style_for_jedi_completion(jc),
                     )
-                except TypeError:
-                    # Issue #9: bad syntax causes completions() to fail in jedi.
-                    # https://github.com/jonathanslenders/python-prompt-toolkit/issues/9
-                    pass
-                except UnicodeDecodeError:
-                    # Issue #43: UnicodeDecodeError on OpenBSD
-                    # https://github.com/jonathanslenders/python-prompt-toolkit/issues/43
-                    pass
-                except AttributeError:
-                    # Jedi issue #513: https://github.com/davidhalter/jedi/issues/513
-                    pass
-                except ValueError:
-                    # Jedi issue: "ValueError: invalid \x escape"
-                    pass
-                except KeyError:
-                    # Jedi issue: "KeyError: u'a_lambda'."
-                    # https://github.com/jonathanslenders/ptpython/issues/89
-                    pass
-                except IOError:
-                    # Jedi issue: "IOError: No such file or directory."
-                    # https://github.com/jonathanslenders/ptpython/issues/71
-                    pass
-                except AssertionError:
-                    # In jedi.parser.__init__.py: 227, in remove_last_newline,
-                    # the assertion "newline.value.endswith('\n')" can fail.
-                    pass
-                except SystemError:
-                    # In jedi.api.helpers.py: 144, in get_stack_at_position
-                    # raise SystemError("This really shouldn't happen. There's a bug in Jedi.")
-                    pass
-                except NotImplementedError:
-                    # See: https://github.com/jonathanslenders/ptpython/issues/223
-                    pass
-                except Exception:
-                    # Supress all other Jedi exceptions.
-                    pass
-                else:
-                    for jc in jedi_completions:
-                        if jc.type == "function":
-                            suffix = "()"
-                        else:
-                            suffix = ""
-
-                        yield Completion(
-                            jc.name_with_symbols,
-                            len(jc.complete) - len(jc.name_with_symbols),
-                            display=jc.name_with_symbols + suffix,
-                            display_meta=jc.type,
-                            style=_get_style_for_name(jc.name_with_symbols),
-                        )
 
 
 class DictionaryCompleter(Completer):
@@ -575,10 +613,15 @@ except ImportError:  # Python 2.
     _builtin_names = []
 
 
-def _get_style_for_name(name: str) -> str:
+def _get_style_for_jedi_completion(jedi_completion) -> str:
     """
     Return completion style to use for this name.
     """
+    name = jedi_completion.name_with_symbols
+
+    if jedi_completion.type == "param":
+        return "class:completion.param"
+
     if name in _builtin_names:
         return "class:completion.builtin"
 
